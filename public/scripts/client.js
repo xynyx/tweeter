1158b3a05d438ec9ac2ef4859fa81e382f8cf4d5
xynyx <matt.taylr22@gmail.com>
2020-03-25 13:21:08 +0000

    Initial create tweet function;

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test code
+const tweetData = {
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac"
+  },
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants"
+  },
+  created_at: 1461116232227
+};
+
+$(() => {
+  const createTweetElement = function(tweet) {
+    // Takes in tweet object (initial-tweets.json) & (/tweets)
+    // Return tweet <article>
+    let $tweet = $("<article>").addClass("tweet");
+
+    
+
+    return $tweet;
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  console.log($tweet);
+});
