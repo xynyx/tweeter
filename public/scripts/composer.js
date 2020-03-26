9c7551d4e0e00e2e6250d5cf97492ab381e3f01e
xynyx <matt.taylr22@gmail.com>
2020-03-26 18:02:52 +0000

    Complete bring-to-tweet button

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,5 +1,7 @@
 // Ensure DOM has loaded first (shortform for $(document).ready(function))
 $(() => {
+  //TODO - MAKE IIFE OR LEAVE?
+  // Check if tweet length is greater than 140 chars
   $("#tweet-text").on("keyup keydown", function() {
     let remaining = $(this).siblings(".counter")[0];
     let textLength = $(this).val().length;
@@ -12,18 +14,40 @@ $(() => {
     }
   });
 
-  (function hideComposeButton() {
+  // Modular function that shows compose tweet area
+  const showCompose = () => {
+    $("#compose-field")
+      .removeClass("invisible")
+      .show({ duration: 150, easing: "swing" });
+    $("#tweet-text").focus();
+  };
+
+  // Button on navbar that closes/opens compose tweet area
+  (function toggleComposeButton() {
     $("#compose").click(() => {
       const composeField = $("#compose-field");
       if (composeField.hasClass("invisible")) {
-        composeField
-          .removeClass("invisible")
-          .show({ duration: 150, easing: "swing" });
-        $("#tweet-text").focus();
+        showCompose();
       } else {
         composeField
           .hide({ duration: 50, easing: "linear" })
           .addClass("invisible");
+      }
+    });
+  })();
+
+  // Fixed button that brings user to top of page & opens compose tweet
+  (function bringToComposeButton() {
+    $("#to-compose").on("click", function() {
+      $(window).scrollTop(0);
+      showCompose();
+    });
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() < 400) {
+        $("#to-compose").hide();
+      } else {
+        $("#to-compose").show();
       }
     });
   })();
